6e7c320d2374bc7650fcffb1f896208e85a18fcb
looncat <147073837+looncat@users.noreply.github.com>
2023-10-12 17:36:27 +0300

    Add files via upload

--- pinponk.py
+++ pinponk.py
@@ -21,27 +21,52 @@ class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
-           self.rect.x -= self.speed
-        if keys[K_s] and self.rect.y < win_width - 80:
-           self.rect.x += self.speed
+           self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
-           self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_width - 80:
-           self.rect.x += self.speed
+           self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
 background = transform.scale(image.load('holst.jpg'), (win_width, win_height))
-Player1 = Player('palca.PNG',0,100,5,75,90)
-Player2 = Player('palca.PNG',500,100,5,75,90)
+player1 = Player('palcka.jpg',0,100,5,50,120)
+player2 = Player('palcka.jpg',650,100,5,50,120)
 ball = GameSprite('shar.png',250,250,5,60,60)
-FPS = 60
+FPS = 90
 clock = time.Clock()
-
+finish = False 
+speed_x = 3
+speed_y = 3
 game = True
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180,0,0))
+font2 = font.Font(None, 35)
+lose2 = font2.render('PLAYER 2 LOSE!', True, (180,0,0))
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.blit(background,(0,0))  
+    window.blit(background,(0,0))
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200,200))
+    if ball.rect.x > 620:
+        finish = True
+        window.blit(lose2, (200,200))
+    if ball.rect.y > win_height -80 or ball.rect.y <0:
+        speed_y *= -1
+    if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+        speed_x *= -1
+    player1.update_l()
+    player2.update_r()
+    player1.reset()
+    player2.reset()
+    ball.reset()
     display.update()
     clock.tick(FPS)
